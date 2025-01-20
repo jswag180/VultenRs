ddb1ff217820cd2a0703c635aedaf2bd8a6a8b5c
jswag180 <jefferycole100@gmail.com>
2025-01-20 11:29:15 -0600

    Fix high vram usage from benches

--- backend/benches/bench_matmul.rs
+++ backend/benches/bench_matmul.rs
@@ -25,72 +25,79 @@ fn bench_square(c: &mut Criterion, inst: &VultenInstance) {
     ];
     for shape in shapes {
         let dims: [i64; 2] = [shape.0, shape.1];
-        let buff_size = dims.iter().product::<i64>() * 4;
-
-        let staging_buff = inst.create_buffer(
-            backend::memory::VultenBufferType::Staging,
-            buff_size as u64,
-            true,
-            false,
-        );
-        let staging = unsafe {
-            std::slice::from_raw_parts_mut(
-                staging_buff.get_mapped_ptr().unwrap() as *mut f32,
-                buff_size as usize / 4,
-            )
-        };
-        for (idx, val) in staging.iter_mut().enumerate() {
-            *val = idx as f32;
-        }
-        let a_buff = inst.create_buffer(
-            backend::memory::VultenBufferType::Device,
-            buff_size as u64,
-            false,
-            true,
-        );
-        let a = KernelInput {
-            buff: KernelBuff::Buff(a_buff.into()),
-            dims: &dims,
-        };
-        let b_buff = inst.create_buffer(
-            backend::memory::VultenBufferType::Device,
-            buff_size as u64,
-            false,
-            true,
-        );
-        let b = KernelInput {
-            buff: KernelBuff::Buff(b_buff.into()),
-            dims: &dims,
-        };
-        let out_buff = inst.create_buffer(
-            backend::memory::VultenBufferType::Device,
-            buff_size as u64,
-            true,
-            false,
-        );
-        let output = KernelInput {
-            buff: KernelBuff::Buff(out_buff.into()),
-            dims: &dims,
-        };
-
-        matmul::matmul_inline_transpose::run(inst, DT_FLOAT, &a, false, &b, false, &output)
-            .unwrap();
-
-        //group.throughput(criterion::Throughput::BytesDecimal(buff_size as u64));
         // (m, p) * (p, n)
         //flops = mn(2p - 1)
-        let flops = shape.0 * shape.0 * ((2 * shape.0) - 1);
+        let flops = dims[0] * dims[0] * ((2 * dims[0]) - 1);
         group.throughput(criterion::Throughput::Elements(flops as u64));
         group.bench_with_input(
             BenchmarkId::from_parameter(shape.0),
             &shape,
             |bench, &_shape| {
-                bench.iter(|| {
-                    matmul::matmul_inline_transpose::run(
-                        inst, DT_FLOAT, &a, false, &b, false, &output,
-                    )
-                    .unwrap();
-                })
+                bench.iter_batched(
+                    || {
+                        let buff_size = dims.iter().product::<i64>() * 4;
+
+                        let staging_buff = inst.create_buffer(
+                            backend::memory::VultenBufferType::Staging,
+                            buff_size as u64,
+                            true,
+                            false,
+                        );
+                        let staging = unsafe {
+                            std::slice::from_raw_parts_mut(
+                                staging_buff.get_mapped_ptr().unwrap() as *mut f32,
+                                buff_size as usize / 4,
+                            )
+                        };
+                        for (idx, val) in staging.iter_mut().enumerate() {
+                            *val = idx as f32;
+                        }
+                        let a_buff = inst.create_buffer(
+                            backend::memory::VultenBufferType::Device,
+                            buff_size as u64,
+                            false,
+                            true,
+                        );
+                        let a = KernelInput {
+                            buff: KernelBuff::Buff(a_buff.into()),
+                            dims: &dims,
+                        };
+                        let b_buff = inst.create_buffer(
+                            backend::memory::VultenBufferType::Device,
+                            buff_size as u64,
+                            false,
+                            true,
+                        );
+                        let b = KernelInput {
+                            buff: KernelBuff::Buff(b_buff.into()),
+                            dims: &dims,
+                        };
+                        let out_buff = inst.create_buffer(
+                            backend::memory::VultenBufferType::Device,
+                            buff_size as u64,
+                            true,
+                            false,
+                        );
+                        let output = KernelInput {
+                            buff: KernelBuff::Buff(out_buff.into()),
+                            dims: &dims,
+                        };
+
+                        matmul::matmul_inline_transpose::run(
+                            inst, DT_FLOAT, &a, false, &b, false, &output,
+                        )
+                        .unwrap();
+
+                        (a, b, output)
+                    },
+                    |(a, b, output)| {
+                        matmul::matmul_inline_transpose::run(
+                            inst, DT_FLOAT, &a, false, &b, false, &output,
+                        )
+                        .unwrap();
+                    },
+                    criterion::BatchSize::PerIteration,
+                )
             },
         );
     }
@@ -114,57 +121,6 @@ fn bench_tall(c: &mut Criterion, inst: &VultenInstance) {
         let a_dims: [i64; 2] = [shape.0, shape.1];
         let b_dims: [i64; 2] = [shape.1, shape.0];
         let c_dims: [i64; 2] = [shape.0, shape.0];
-        let ab_buff_size = a_dims.iter().product::<i64>() * 4;
-        let c_buff_size = c_dims.iter().product::<i64>() * 4;
-
-        let staging_buff = inst.create_buffer(
-            backend::memory::VultenBufferType::Staging,
-            ab_buff_size as u64,
-            true,
-            false,
-        );
-        let staging = unsafe {
-            std::slice::from_raw_parts_mut(
-                staging_buff.get_mapped_ptr().unwrap() as *mut f32,
-                ab_buff_size as usize / 4,
-            )
-        };
-        for (idx, val) in staging.iter_mut().enumerate() {
-            *val = idx as f32;
-        }
-        let a_buff = inst.create_buffer(
-            backend::memory::VultenBufferType::Device,
-            ab_buff_size as u64,
-            false,
-            true,
-        );
-        let a = KernelInput {
-            buff: KernelBuff::Buff(a_buff.into()),
-            dims: &a_dims,
-        };
-        let b_buff = inst.create_buffer(
-            backend::memory::VultenBufferType::Device,
-            ab_buff_size as u64,
-            false,
-            true,
-        );
-        let b = KernelInput {
-            buff: KernelBuff::Buff(b_buff.into()),
-            dims: &b_dims,
-        };
-        let out_buff = inst.create_buffer(
-            backend::memory::VultenBufferType::Device,
-            c_buff_size as u64,
-            true,
-            false,
-        );
-        let output = KernelInput {
-            buff: KernelBuff::Buff(out_buff.into()),
-            dims: &c_dims,
-        };
-
-        matmul::matmul_inline_transpose::run(inst, DT_FLOAT, &a, false, &b, false, &output)
-            .unwrap();
 
         //group.throughput(criterion::Throughput::BytesDecimal(buff_size as u64));
         // (m, p) * (p, n)
@@ -175,12 +131,71 @@ fn bench_tall(c: &mut Criterion, inst: &VultenInstance) {
             BenchmarkId::from_parameter(shape.0),
             &shape,
             |bench, &_shape| {
-                bench.iter(|| {
-                    matmul::matmul_inline_transpose::run(
-                        inst, DT_FLOAT, &a, false, &b, false, &output,
-                    )
-                    .unwrap();
-                })
+                bench.iter_batched(
+                    || {
+                        let ab_buff_size = a_dims.iter().product::<i64>() * 4;
+                        let c_buff_size = c_dims.iter().product::<i64>() * 4;
+
+                        let staging_buff = inst.create_buffer(
+                            backend::memory::VultenBufferType::Staging,
+                            ab_buff_size as u64,
+                            true,
+                            false,
+                        );
+                        let staging = unsafe {
+                            std::slice::from_raw_parts_mut(
+                                staging_buff.get_mapped_ptr().unwrap() as *mut f32,
+                                ab_buff_size as usize / 4,
+                            )
+                        };
+                        for (idx, val) in staging.iter_mut().enumerate() {
+                            *val = idx as f32;
+                        }
+                        let a_buff = inst.create_buffer(
+                            backend::memory::VultenBufferType::Device,
+                            ab_buff_size as u64,
+                            false,
+                            true,
+                        );
+                        let a = KernelInput {
+                            buff: KernelBuff::Buff(a_buff.into()),
+                            dims: &a_dims,
+                        };
+                        let b_buff = inst.create_buffer(
+                            backend::memory::VultenBufferType::Device,
+                            ab_buff_size as u64,
+                            false,
+                            true,
+                        );
+                        let b = KernelInput {
+                            buff: KernelBuff::Buff(b_buff.into()),
+                            dims: &b_dims,
+                        };
+                        let out_buff = inst.create_buffer(
+                            backend::memory::VultenBufferType::Device,
+                            c_buff_size as u64,
+                            true,
+                            false,
+                        );
+                        let output = KernelInput {
+                            buff: KernelBuff::Buff(out_buff.into()),
+                            dims: &c_dims,
+                        };
+
+                        matmul::matmul_inline_transpose::run(
+                            inst, DT_FLOAT, &a, false, &b, false, &output,
+                        )
+                        .unwrap();
+                        (a, b, output)
+                    },
+                    |(a, b, output)| {
+                        matmul::matmul_inline_transpose::run(
+                            inst, DT_FLOAT, &a, false, &b, false, &output,
+                        )
+                        .unwrap();
+                    },
+                    criterion::BatchSize::PerIteration,
+                )
             },
         );
     }
